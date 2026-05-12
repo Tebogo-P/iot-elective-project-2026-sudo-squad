2b20492ab118445071d0fc4be0deea585f8ebe5e
Tebogo <notorioustee11@gmail.com>
2026-05-12 13:01:07 +0200

    AttendanceLog JSON intergration

--- src/main/java/za/ac/cput/sudosquadattendancesystem/domain/AttendanceLog.java
+++ src/main/java/za/ac/cput/sudosquadattendancesystem/domain/AttendanceLog.java
@@ -1,11 +1,12 @@
 package za.ac.cput.sudosquadattendancesystem.domain;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
 
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "attendance_logs")
@@ -29,6 +30,7 @@ public class AttendanceLog {
     private Integer fingerprintId;
 
     @Column(name = "scan_timestamp", nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime scanTimestamp;
 
     @Column(name = "access_granted", nullable = false)
